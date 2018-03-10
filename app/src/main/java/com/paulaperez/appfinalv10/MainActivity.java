199d5cf3c1e2ff069126bb26ab216a6101eefffb
Bhumi Shah <paulaperezt16@gmail.com>
2018-03-10 14:09:08 -0500

    Llamado de mnue en el java

--- app/src/main/java/com/paulaperez/appfinalv10/MainActivity.java
+++ app/src/main/java/com/paulaperez/appfinalv10/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +16,23 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+        switch ( item.getItemId() ) {
+            case R.id.action_profile:
+                Toast.makeText(MainActivity.this, "HOlita bebe", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_logout:
+                return true;
+
+            default:
+
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
