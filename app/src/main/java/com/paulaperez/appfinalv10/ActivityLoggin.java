217c51dc2a0d71ea92c71d658a572f2b7bb35799
Bhumi Shah <paulaperezt16@gmail.com>
2018-03-10 15:48:07 -0500

    Se definio en el activity loggin la tv register el paso actividades

--- app/src/main/java/com/paulaperez/appfinalv10/ActivityLoggin.java
+++ app/src/main/java/com/paulaperez/appfinalv10/ActivityLoggin.java
@@ -1,13 +1,36 @@
 package com.paulaperez.appfinalv10;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ActivityLoggin extends AppCompatActivity {
+
+    private EditText etuser, etpass;
+    private Button btlog;
+    private TextView tvregister;
+    public static final int LOGGINREQUESTCODE=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loggin);
+        etuser=(EditText)findViewById(R.id.loggin_etUser);
+        etpass=(EditText)findViewById(R.id.loggin_etPass);
+        btlog=(Button)findViewById(R.id.loggin_btLogin);
+        tvregister=(TextView)findViewById(R.id.loggin_tvRegister);
+
+        tvregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(ActivityLoggin.this,ActivityRegistration.class);
+                startActivityForResult(i,LOGGINREQUESTCODE);
+            }
+        });
     }
+
 }
