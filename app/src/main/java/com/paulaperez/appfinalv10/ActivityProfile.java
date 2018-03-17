4dfddb1df0b528ae37ed1330cc6175e5c7f5440e
Bhumi Shah <paulaperezt16@gmail.com>
2018-03-17 14:21:57 -0500

    Ya se completo la aplicacion en un obra gris, se valido los usuarios en loggin, ademas se pasaron los datos entre actividades en forma correcta

--- app/src/main/java/com/paulaperez/appfinalv10/ActivityProfile.java
+++ app/src/main/java/com/paulaperez/appfinalv10/ActivityProfile.java
@@ -5,14 +5,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ActivityProfile extends AppCompatActivity {
+
+    private String username, password, name, email;
+    private TextView tvData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        tvData=(TextView)findViewById(R.id.tvUserData);
+
+        Bundle fromLoggin= getIntent().getExtras();
+        if(fromLoggin!=null){
+            username=fromLoggin.getString(ActivityRegistration.intent_User, "");
+            password=fromLoggin.getString(ActivityRegistration.intent_pass, "");
+            name=fromLoggin.getString(ActivityRegistration.intent_Name, "");
+            email=fromLoggin.getString(ActivityRegistration.intent_Email, "");
+            //Toast.makeText(ActivityProfile.this, "me acabaron de mandar:"+username+"-"+password,Toast.LENGTH_LONG).show();
+
+            printResults();
+
+//TODO Mejorar la letra
+        }
+
+
+
+
     }
 
 
@@ -28,6 +51,10 @@ public class ActivityProfile extends AppCompatActivity {
             case R.id.action_principal:
                 Toast.makeText(ActivityProfile.this, "Holita principal", Toast.LENGTH_SHORT).show();
                 Intent i= new Intent(ActivityProfile.this, MainActivity.class);
+                i.putExtra(ActivityRegistration.intent_User, username);
+                i.putExtra(ActivityRegistration.intent_pass, password);
+                i.putExtra(ActivityRegistration.intent_Name, name);
+                i.putExtra(ActivityRegistration.intent_Email, email);
                 startActivity(i);
                 finish();
 
@@ -36,6 +63,11 @@ public class ActivityProfile extends AppCompatActivity {
             case R.id.action_logout:
 
                 Intent log= new Intent(ActivityProfile.this, ActivityLoggin.class);
+                log.putExtra(ActivityRegistration.intent_User, username);
+                log.putExtra(ActivityRegistration.intent_pass, password);
+                log.putExtra(ActivityRegistration.intent_Name, name);
+                log.putExtra(ActivityRegistration.intent_Email, email);
+
                 startActivity(log);
                 finish();
                 return true;
@@ -44,5 +76,16 @@ public class ActivityProfile extends AppCompatActivity {
 
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void printResults(){
+        String results="";
+
+        results+=getString(R.string.name)+":"+name+"\n";
+        results+=getString(R.string.user)+":"+username+"\n";
+        results+=getString(R.string.pass)+":"+password+"\n";
+        results+=getString(R.string.email)+":"+email+"\n";
+
+        tvData.setText(results);
     }
 }
